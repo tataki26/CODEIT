ffc8f92629f68640689d57d6e5be608ad7c95cd9
tataki26 <72514622+tataki26@users.noreply.github.com>
2021-01-27 15:09:17 +0900

    Update voca_quiz.py
    영한 퀴즈에서 한영 퀴즈로 변경

--- voca_quiz.py
+++ voca_quiz.py
@@ -2,10 +2,10 @@ with open('vocabulary.txt', 'r', encoding="utf-8") as f:
     for line in f:
         voca_list = line.strip().split(": ")
 
-        englsh_word = input(f"{voca_list[0]}: ")
-        korean_word = voca_list[1]
+        korean_word = input(f"{voca_list[1]}: ")
+        english_word = voca_list[0]
 
-        if englsh_word == korean_word:
+        if korean_word == english_word:
             print("맞았습니다!")
         else:
-            print(f"아쉽습니다. 정답은 {korean_word}입니다.")
+            print(f"아쉽습니다. 정답은 {english_word}입니다.")
